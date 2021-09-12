c35301755c34d017dc7947ae0becb30418c63895
Mike Warot <chezmike@gmail.com>
2021-09-11 21:48:13 -0500

    start build out user interface

--- watch_hn_mainform.pas
+++ watch_hn_mainform.pas
@@ -5,10 +5,19 @@ unit watch_hn_mainform;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    EditThreadID: TEdit;
+    Label1: TLabel;
+    Memo1: TMemo;
+    TreeView1: TTreeView;
+    procedure Button1Click(Sender: TObject);
   private
 
   public
@@ -21,6 +30,17 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  s : string;
+begin
+  Memo1.Clear;
+  s := 'Fetching contents of Thread #' + EditThreadID.Text + ' via the web';
+  Memo1.Append(S);
+end;
 
 end.
 
