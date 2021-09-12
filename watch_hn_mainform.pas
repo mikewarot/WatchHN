b4440914499af78c2ce2257e475be6900d02d405
Mike Warot <chezmike@gmail.com>
2021-09-11 22:34:47 -0500

    first version which actually retrieves some data correctly
    add ssl support, properly initialize http1 client object

--- watch_hn_mainform.pas
+++ watch_hn_mainform.pas
@@ -5,7 +5,9 @@ unit watch_hn_mainform;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls, fphttpclient, openssl,
+  opensslsockets;
+
 
 type
 
@@ -18,6 +20,7 @@ type
     Memo1: TMemo;
     TreeView1: TTreeView;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -26,6 +29,8 @@ type
 
 var
   Form1: TForm1;
+  Http1 : TFPHttpClient;
+
 
 implementation
 
@@ -36,10 +41,23 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   s : string;
+  url : string;
+  r : string;
 begin
   Memo1.Clear;
   s := 'Fetching contents of Thread #' + EditThreadID.Text + ' via the web';
   Memo1.Append(S);
+
+  url := 'https://hacker-news.firebaseio.com/v0/item/'+EditThreadID.Text+'.json?print=pretty';
+  Memo1.Append('Fetching URL: '+url);
+  r := http1.Get(url);
+  memo1.Append(r);
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  HTTP1 := TFPHttpClient.Create(nil);
 end;
 
 end.
